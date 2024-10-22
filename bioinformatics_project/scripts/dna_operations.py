3319ae3153ce7d90564d5d4c2062ad5776e86558
Shwetha Kandhalu <159231606+shwe-kandhalu@users.noreply.github.com>
2024-10-22 22:23:56 +0000

    question 3

---
+++ bioinformatics_project/scripts/dna_operations.py
@@ -0,0 +1,52 @@
+"""
+
+DNA operations file that does the following:
+
+complement(sequence): Returns the complement of a DNA sequence 
+(A -> T, C -> G, G -> C, T -> A; e.g., "CCTCAGC" -> "GGAGTCG").
+
+reverse(sequence): Returns the reverse of a sequence (e.g. "CCTCAGC" -> "CGACTCC").
+
+reverse_complement(sequence): Returns the reverse complement of a DNA sequence (e.g. "CCTCAGC" -> "GCTGAGG"); 
+i.e. the reverse of the complement (apply complement then reverse, or vice versa).
+
+Command line input: python dna_operations.py GAATTC
+
+"""
+
+import argparse
+
+complements = {
+    "A":"T", 
+    "T": "A",
+    "G":"C",
+    "C": "G"
+}
+
+def complement(sequence): # returns complement of DNA sequence
+    complement_sequence = ""
+    for n in sequence:
+        complement_sequence += complements[n]
+    return complement_sequence
+
+def reverse(sequence): # returns reverse of DNA sequence
+    reverse_sequence = sequence[::-1] 
+    return reverse_sequence
+
+def reverse_complement(sequence): # returns reverse complement of DNA sequence
+    complement_sequence = complement(sequence)
+    reverse_of_comp = reverse(complement_sequence)
+    return reverse_of_comp
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sequence")
+    args = parser.parse_args()
+
+    complement_sequence = complement(args.sequence)
+    reverse_sequence = reverse(args.sequence)
+    reverse_of_comp = reverse_complement(args.sequence)
+
+	print(f"Original sequence: {args.sequence} /n Complement: {comp} /n Reverse: {rev} /n Reverse complement: {revcomp}")
+
+
