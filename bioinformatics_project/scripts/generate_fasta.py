253011171bae85434d15e05b1cc95d889380d1dd
Shwetha Kandhalu <159231606+shwe-kandhalu@users.noreply.github.com>
2024-10-22 21:24:39 +0000

    question 2

--- bioinformatics_project/scripts/generate_fasta.py
+++ bioinformatics_project/scripts/generate_fasta.py
@@ -11,12 +11,11 @@ import textwrap
 
 # function to generate random dna sequence and save as fasta file with 80 bases per line
 def generate_random_dna_seq(length = 1000000):
-    base = ['A', 'T', 'C', 'G'] # base options
+    base = ("A", "T", "C", "G") # base options
     sequence = "".join(random.choices(base, k = length)) # keep picking random bases till defined length of 1000000
     
     with open('/workspaces/05-first-exam-shwe-kandhalu/bioinformatics_project/data/random_sequence.fasta', 'w') as file:
-        for bases in range(0, len(sequence), 80): # makes sure there are 80 bases per line
-            line = sequence[base:base+80]
+        for line in textwrap.wrap(sequence, 80):
             file.write(line + '\n')
 
     print(f"Random DNA sequence generated and saved to bioinformatics_project/data/random_sequence.fasta")
